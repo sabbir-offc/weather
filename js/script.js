2178bc57cf2c7a2355a7bcdb5a21b7ead2f4fee9
sabbirofc <mdsabbirhowlader420@gmail.com>
2023-09-06 17:38:20 +0600

    enter button formulla added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`
 const searchBox = document.getElementById('search-box');
 const searchButton = document.getElementById('search-btn');
 
+searchBox.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+
+        searchButton.click();
+    }
+});
+
 const weatherImage = (weather) => {
     // console.log(weather)
     if (weather === 'Haze') {
@@ -33,24 +41,24 @@ const getWeather = async (city) => {
     else {
         weather.innerHTML = `
     <div>
-                        <img draggable="false" src="${weatherImage(data.weather[0].main)}" class="mx-auto my-6" id="weather-icon" alt="">
-                        <h1 class="text-7xl font-medium" id="temp">${Math.round(data.main.temp)}°C</h1>
-                        <p class="text-xl mb-3">${data.weather[0].description}</p>
-                        <h2 class="text-2xl font-normal my-3">Feels Like ${data.main.feels_like}</h2>
-                        <h2 class="text-5xl font-normal mt-3">${data.name}</h2>
+                        <img draggable="false" src="${weatherImage(data.weather[0].main)}" class="mx-auto my-6 w- md:w-fit" id="weather-icon" alt="">
+                        <h1 class="text-5xl md:text-7xl font-medium" id="temp">${Math.round(data.main.temp)}°C</h1>
+                        <p class="text-lg md:text-xl mb-3">${data.weather[0].description}</p>
+                        <h2 class="text-xl md:text-2xl font-normal my-3">Feels Like ${Math.round(data.main.feels_like)}°C</h2>
+                        <h2 class="text-3xl md:text-4xl lg:text-5xl font-normal mt-3">${data.name}</h2>
                     </div>
-                    <div id="details" class="flex items-center justify-between px-10 mt-12">
+                    <div id="details" class="flex items-center justify-between px-10 mt-12 flex-col md:flex-row gap-3">
                         <div class="flex items-center text-left">
-                            <img src="./images/humidity.png" alt="" class="mx-auto w-10 mr-2">
+                            <img src="./images/humidity.png" alt="" class="mx-auto w-8 md:w-10 mr-2">
                             <div>
-                                <p class="text-2xl">${data.main.humidity}%</p>
+                                <p class="text-xl md:text-2xl">${data.main.humidity}%</p>
                                 <p>Humidity</p>
                             </div>
                         </div>
                         <div class="flex items-center text-left">
-                            <img src="./images/wind.png" alt="" class="mx-auto w-10 mr-2">
+                            <img src="./images/wind.png" alt="" class="mx-auto w-8 md:w-10 mr-2">
                             <div>
-                                <p class="text-2xl">${data.wind.speed} km/h</p>
+                                <p class="text-xl md:text-2xl">${data.wind.speed} km/h</p>
                                 <p>Wind Speed</p>
                             </div>
                         </div>
@@ -75,4 +83,4 @@ getWeather('bangladesh')
 
 document.addEventListener("contextmenu", (event) => {
     event.preventDefault();
-   });
+});
